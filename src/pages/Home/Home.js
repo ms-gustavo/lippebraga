1924b5a5a98fd4f29dd94163d01649644870cce0
Gustavo Maia <ms.gustavo@outlook.com>
2023-03-13 22:01:45 -0300

    Mudanca texto home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <p>
           Conheça Lippe Braga, a nova voz do sertanejo e piseiro! Com um talento
           inegável e uma paixão pela música que transborda em cada apresentação,
-          Lippe Braga mescla clássicos do sertanejo e hits do piseiro, suas
+          Lippe Braga mescla clássicos do sertanejo e hits do piseiro. Suas
           apresentações são uma verdadeira festa que prometem animar qualquer
           público. Venha se encantar com a voz marcante e a energia contagiante
           de Lippe Braga - o futuro da música sertaneja!
